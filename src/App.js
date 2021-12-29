9f885d93e4bdb5e2b0f5f19cbcf5648e50d5c6cf
Julian Swerdlin <julianswer36@gmail.com>
2021-12-30 00:49:07 +0100

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
+import {Container} from '@mui/material';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TaskList/>} />
-        <Route path="/task/new" element={<TaskForm/>} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<TaskList/>} />
+          <Route path="/task/new" element={<TaskForm/>} />
+          {/* <Route path="/task/:id" element={<TaskForm/>} /> */}
+        </Routes>
+      </Container>
     </BrowserRouter>
   )
 }
