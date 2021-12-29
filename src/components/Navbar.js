3f7f6146770b68d914ab2922d6921963ff2b1d36
Julian Swerdlin <julianswer36@gmail.com>
2021-12-30 00:57:55 +0100

    front files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,25 @@
-import {Button} from '@mui/material';
+import {AppBar, Box, Button, Container, Toolbar, Typography} from '@mui/material';
+import {Link, useNavigate} from 'react-router-dom';
 
 export default function Navbar() {
+
+    const navigate = useNavigate()
+
     return (
-        <div>
-            navbar
-            <Button variant="contained" color='secondary'>Click me</Button>
-        </div>
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position='static' color='transparent'>
+                <Container>
+                    <Toolbar>
+                        <Typography sx={{flexGrow: 1}}>
+                            <Link to='/'>PERN Stack</Link>
+                        </Typography>
+
+                        <Button variant='contained' color='primary' onClick={() => navigate('/task/new')}>
+                            New Task
+                        </Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </Box>
     )
 }
