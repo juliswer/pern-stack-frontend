496bbc4f05a2aea7000eae2305661e18f26954dc
Julian Swerdlin <julianswer36@gmail.com>
2021-12-30 01:04:52 +0100

    front files

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,7 +1,25 @@
+import {Card, CardContent, Grid, TextField, Typography} from '@mui/material';
+
 export default function TaskForm() {
     return (
-        <div>
-            task form
-        </div>
+        <Grid container direction="column" alignItems="center" justifyContent="center">
+            <Grid item xs={3}>
+                <Card
+                    sx={{mt: 5}}
+                >
+                    <Typography>
+                        Create Task
+                    </Typography>
+                    <CardContent>
+                        <form>
+                            <TextField 
+                                variant='filled' 
+                                label='Write your title'
+                            />
+                        </form>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     )
 }
