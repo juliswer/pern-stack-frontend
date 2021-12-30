fc0d48467181a1f899ceca950d5bab1715a39d80
Julian Swerdlin <julianswer36@gmail.com>
2021-12-30 01:34:52 +0100

    front files

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,14 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 export default function TaskList() {
 
     const loadTasks = async () => {
         const res = await fetch('http://localhost:4000/tasks');
         const data = await res.json();
-        console.log(data);
+        setTasks(data);
     }
+
+    const [tasks, setTasks] = useState({})
     
     useEffect(() => {
         loadTasks();
